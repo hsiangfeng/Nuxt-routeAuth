bd5dd7e24c35dffe8c7006e7cf82af57496d08d2
hsiangfeng <loveyou520babytw@gmail.com>
2019-08-16 13:36:32 +0800

    first

---
+++ pages/auth/admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>這是需要驗證的頁面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'routerAuth',
+  meta: {
+    requiresAuth: true
+  }
+}
+</script>
---
+++ pages/auth/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>這是登入頁面</h1>
+  </div>
+</template>
